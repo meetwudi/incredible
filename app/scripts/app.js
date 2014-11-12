2c6fcfcb2f9bbc41b5425347b71d658ff2ab8ef6
John Wu <webmaster@leapoahead.com>
2014-11-12 18:20:48 +0800

    frameless window

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,4 +2,12 @@ angular.module('incredible', ['incredible.controllers',
   'incredible.services',
   'incredible.directives',
   'incredible.routes',
-  'ui.bootstrap']);
+  'ui.bootstrap'])
+
+
+.run(function($rootScope) {
+  $rootScope.application = {};
+  $rootScope.application.close = function() {
+    require('nw.gui').Window.get().close(true);
+  };
+});
